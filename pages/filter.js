30cfbcbc3296888976c7f088763cc33803b58529
iamdjoky <maude2413@hotmail.fr>
2022-06-21 13:17:40 +0200

    ajout

--- pages/filter.js
+++ pages/filter.js
@@ -1,18 +1,20 @@
 
+import Image from 'next/image'
+
 export default function Research() {
     return ( 
-        <div class="ml-8 "> 
+        <div class="ml-8 w-45"> 
             
-            <button class="absolute top-4 left-4 text-center text-green border-solid border-2 border-green border-opacity-10 text-18 px-4 py-3 rounded-round">X</button>
+            <button class="absolute top-4 left-4 text-center text-green border-solid border-2 border-green border-opacity-10 text-18 px-3_5 py-3 rounded-round"><Image  src="/croix.svg" alt="site logo" width={16} height={16} /></button>
 
-            <h1 class="mt-14 text-center text-32 text-green">PAR TYPE DE VIN</h1>
+            <h1 class="mt-20 text-center text-32 text-green">PAR TYPE DE VIN</h1>
                 
         <div class="text-center">
-            <h4 class="text-16 ml-2 mt-6 text-green border-solid border-2 border-green px-3 py-1 rounded-2_5 inline-block">Rouge</h4>
-            <h4 class="text-16 ml-2 text-green border-solid border-2 border-green px-3 py-1 rounded-2_5 inline-block">Blanc</h4>
-            <h4 class="text-16 ml-2 text-green border-solid border-2 border-green px-3 py-1 rounded-2_5 inline-block">Rosé</h4>
-            <h4 class="text-16 ml-2 text-green border-solid border-2 border-green px-3 py-1 rounded-2_5 inline-block">Orange</h4>
-            <h4 class="text-16 ml-2 text-green border-solid border-2 border-green px-3 py-1 rounded-2_5 inline-block">Pet'Nat</h4>
+            <h4 class="text-18 ml-2 mt-6 text-green border-solid border-2 border-green px-3 py-1 rounded-2_5 inline-block">Rouge</h4>
+            <h4 class="text-18 ml-2 text-green border-solid border-2 border-green px-3 py-1 rounded-2_5 inline-block">Blanc</h4>
+            <h4 class="text-18 ml-2 text-green border-solid border-2 border-green px-3 py-1 rounded-2_5 inline-block">Rosé</h4>
+            <h4 class="text-18 ml-2 text-green border-solid border-2 border-green px-3 py-1 rounded-2_5 inline-block">Orange</h4>
+            <h4 class="text-18 ml-2 text-green border-solid border-2 border-green px-3 py-1 rounded-2_5 inline-block">Pet'Nat</h4>
 
 
         </div>
---
+++ pages/section6.js
@@ -0,0 +1,22 @@
+
+export default function Section() {
+    return ( 
+     <div class="bg-yellow min-h-screen width-screen">
+        
+        <button class="ml-40 mt-4 text-center bg-green text-white text-12 px-2 py-1 rounded-2_5">LA CAVE EXCLUSIVE</button>
+
+
+        <h1 class="mt-2  ml-40 text-56 text-black2 w-32 text-center ">NOS BOUTEILLES À L'UNITÉ</h1>
+
+        <h4 class="ml-40 text-center w-30 text-black2 text-14">Il vous est possible de commander une bouteille à l'unité afin de pouvoir découvrir des cuvées différentes ou ré-apprécier des vins qui vous ont marqué.</h4>
+
+        
+        <div class="ml-40">
+            <button class="ml-30 mt-4 text-center bg-red text-white text-14 px-4 py-2 rounded-2_5">VOIR TOUTE LA CAVE EXCLUSIVE</button>
+        
+        </div>
+
+           
+    </div>
+    );
+}   
